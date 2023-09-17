08af927760a13fcc7922cc80dced2ecca373e337
TomAndersen <1040994898@qq.com>
2023-09-18 04:11:24 +0800

    update

--- MySQL/leetcode/LeetCode176.sql
+++ MySQL/leetcode/LeetCode176.sql
@@ -8,15 +8,46 @@ insert into Employee (id, salary) values ('3', '300');
 */
 
 /**
-  1. Using window function
+  1. Window function
  */
 
 
 /**
-  2. Using order by and limit
+  2. ORDER BY and LIMIT-OFFSET in Scalar Subquery
+ */
+SELECT (
+    SELECT DISTINCT
+        salary AS SecondHighestSalary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
+/**
+  3. ORDER BY and LIMIT-OFFSET in subquery
  */
 SELECT
-    salary AS SecondHighestSalary
+    SecondHighestSalary
+FROM (
+    SELECT DISTINCT
+        salary AS SecondHighestSalary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 offset 1
+) AS tmp
+UNION (
+    SELECT null FROM DUAL
+)
+LIMIT 1;
+
+/*
+ 4. Using MAX in scalar subquery of where clause
+ */
+SELECT DISTINCT
+    MAX(salary) AS SecondHighestSalary
 FROM Employee
-ORDER BY salary DESC
-LIMIT 1 offset 1;
+WHERE salary < (
+    SELECT
+        MAX(salary) AS maxSalary
+    FROM Employee
+);
